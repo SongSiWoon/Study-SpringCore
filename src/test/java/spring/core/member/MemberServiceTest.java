810997c9ed17ac59412bd9c85469c134b73cc070
SongSiWoon <siwoons7319@gmail.com>
2021-12-30 02:05:31 +0900

    create AppConfig, apply DIP

--- src/test/java/spring/core/member/MemberServiceTest.java
+++ src/test/java/spring/core/member/MemberServiceTest.java
@@ -1,12 +1,20 @@
 package spring.core.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import spring.core.AppConfig;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
 
+    // test 실행전 실행되는 로직직
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     @Test
     void join(){
         // given
