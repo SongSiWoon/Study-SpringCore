81b55f3a1ad20319dd1c9217435c55cd6932d58f
SongSiWoon <siwoons7319@gmail.com>
2022-03-30 00:29:26 +0900

    [FEAT]: Test AllBean

--- Spring-Core/src/main/java/spring/core/discount/RateDiscountPolicy.java
+++ Spring-Core/src/main/java/spring/core/discount/RateDiscountPolicy.java
@@ -1,5 +1,6 @@
 package spring.core.discount;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import spring.core.annotation.MainDiscountPolicy;
 import spring.core.member.Grade;
@@ -7,6 +8,7 @@ import spring.core.member.Member;
 
 @Component
 @MainDiscountPolicy
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountPercent = 10;
